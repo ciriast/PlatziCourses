e52b42067fa29ef068d1c72f07fb74cd895a9127
cCarlos <ciriastorres@gmail.com>
2024-03-07 19:28:21 -0600

    Add the undefined.ts

---
+++ TypeScript/src/undefined.ts
@@ -0,0 +1,10 @@
+(() => {
+  ///let myNumber: number = undefined;
+  ///let myString: string = null;
+  let myNull: null = null;
+  let myUndefined: undefined = undefined;
+
+  let myNumber: number | null = null;
+
+  let myString: string | undefined = undefined;
+})();
