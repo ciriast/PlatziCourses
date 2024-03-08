cb7003d43070127ce1a6b6253181879b6efb1af1
cCarlos <ciriastorres@gmail.com>
2024-03-08 06:04:23 -0600

    Add the func-objs.ts

---
+++ TypeScript/src/func-objs.ts
@@ -0,0 +1,12 @@
+(() => {
+  const login = (data: {email: string, password: string}) => {
+
+  }
+
+  const myData = {
+    email: "something@hey.com",
+    password: "myPassword"
+  }
+
+  login(myData);
+})();
